54c068188a05dafa29501baaea752f596c065c69
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-12-09 01:37:27 +0200

    feat: import roboto font sizes

--- src/front-end/src/main.jsx
+++ src/front-end/src/main.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import Home from "./pages/Home/Home";
 import Map from "./pages/Map/Map";
 import "./index.css";
