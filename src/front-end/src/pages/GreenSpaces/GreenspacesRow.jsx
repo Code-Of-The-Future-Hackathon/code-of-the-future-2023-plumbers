8d6f5259ab1b00761e3eabcd0c8cf40787c5f353
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2023-12-09 17:15:00 +0200

    clear

--- src/front-end/src/pages/GreenSpaces/GreenspacesRow.jsx
+++ src/front-end/src/pages/GreenSpaces/GreenspacesRow.jsx
@@ -9,12 +9,12 @@ import {
   } from '@mui/material';
   import { Box } from '@mui/system';
   import { FormatShapes, ModeEdit } from '@mui/icons-material';
-  import { setPublicAreas } from 'redux/publicAreas/slice';
+   
   import { useDispatch } from 'react-redux';
   import { useNavigate } from 'react-router';
-  import { setCenter } from 'redux/center/slice';
-  import { setZoom } from 'redux/zoom/slice';
-  import { setLoader } from 'redux/loader/slice';
+  import { setMapCenter } from 'redux/setMapCenterSlice';
+  import { setMapZoomLevel } from 'redux/mapZoomLevelSlice';
+  import { setIsLoading } from 'redux/loader/slice';
   import { useState } from 'react';
   import { changePublicAreaGroup } from 'services/areasOfPublicUse';
   import { setNotification } from 'redux/notification/slice';
@@ -24,74 +24,52 @@ import {
     const dispatch = useDispatch();
     const navigate = useNavigate();
   
-    const onZoneIconClick = async () => {
-      dispatch(setLoader(true));
+    // const onZoneIconClick = async () => {
+    //   dispatch(setLoader(true));
   
-      if (record.zone) {
-        const center = {
-          lat: record.zone.points[0].lat,
-          lng: record.zone.points[0].lng,
-        };
+    //   if (record.zone) {
+    //     const center = {
+    //       lat: record.zone.points[0].lat,
+    //       lng: record.zone.points[0].lng,
+    //     };
   
-        const zonePointsBody = record.zone.points.map((point) => ({
-          Lat: point.lat,
-          Lng: point.lng,
-        }));
+    //     const zonePointsBody = record.zone.points.map((point) => ({
+    //       Lat: point.lat,
+    //       Lng: point.lng,
+    //     }));
   
-        try {
-          const response = await fetch(
-            `https://europe-west1-bins-to-owners-332bc.cloudfunctions.net/getZoneCenter`,
-            {
-              method: 'POST',
-              body: JSON.stringify({
-                ZonePoints: zonePointsBody,
-              }),
-            }
-          );
+    //     try {
+    //       const response = await fetch(
+    //         `https://europe-west1-bins-to-owners-332bc.cloudfunctions.net/getZoneCenter`,
+    //         {
+    //           method: 'POST',
+    //           body: JSON.stringify({
+    //             ZonePoints: zonePointsBody,
+    //           }),
+    //         }
+    //       );
   
-          const resultCenter = await response.json();
+    //       const resultCenter = await response.json();
   
-          if (resultCenter) {
-            center.lat = resultCenter.Lat;
-            center.lng = resultCenter.Lng;
-          }
-        } catch (error) {
-          console.error(error);
-        } finally {
-          dispatch(setLoader(false));
-        }
+    //       if (resultCenter) {
+    //         center.lat = resultCenter.Lat;
+    //         center.lng = resultCenter.Lng;
+    //       }
+    //     } catch (error) {
+    //       console.error(error);
+    //     } finally {
+    //       dispatch(setLoader(false));
+    //     }
   
-        dispatch(setCenter(center));
-        dispatch(setZoom(20));
-      }
+    //     dispatch(setCenter(center));
+    //     dispatch(setZoom(20));
+    //   }
   
-      dispatch(setPublicAreas([record]));
-      navigate('/');
-    };
+    //   dispatch(setGreenspaces([record]));
+    //   navigate('/');
+    // };
   
-    const onChangeGroup = async (e) => {
-      dispatch(setLoader(true));
-  
-      try {
-        await changePublicAreaGroup(record.id, { group: e.target.value });
-        await getNewRecords();
-        setCurrentGroup(e.target.value);
-  
-        dispatch(
-          setNotification({
-            open: true,
-            message: 'Успешно сменихте групата!',
-            severity: 'success',
-            vertical: 'bottom',
-            horizontal: 'center',
-          })
-        );
-      } catch (error) {
-        console.error(error);
-      } finally {
-        dispatch(setLoader(false));
-      }
-    };
+    
   
     return (
       <TableRow
@@ -102,26 +80,7 @@ import {
         }}
       >
         <TableCell>{record.name}</TableCell>
-        <TableCell>
-          <Box sx={{ minWidth: 120 }}>
-            <FormControl fullWidth>
-              <InputLabel id="demo-simple-select-label">Група</InputLabel>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={currentGroup}
-                label="Група"
-                onChange={onChangeGroup}
-              >
-                {groups.map((group) => (
-                  <MenuItem key={group} value={group}>
-                    {group}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          </Box>
-        </TableCell>
+        
         <TableCell>{record.area}</TableCell>
         <TableCell>{record?.zone?.area?.toFixed(3) || '-'}</TableCell>
         <TableCell>{record.category}</TableCell>
