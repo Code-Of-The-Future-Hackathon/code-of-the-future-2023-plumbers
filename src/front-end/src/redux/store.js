2702bdd201e77116a023f9e09d91f12e76a070b1
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-12-09 15:05:17 +0200

    feat: create slice for is loading

--- src/front-end/src/redux/store.js
+++ src/front-end/src/redux/store.js
@@ -3,11 +3,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import zoomLevelReducer from "./zoomLevelSlice.js";
 import splitMapScreenReducer from "./splitMapScreenSlice.js";
 import mapCenterReducer from "./mapCenterSlice.js";
+import isLoadingReducer from "./isLoadingSlice.js";
 
 export default configureStore({
   reducer: {
     zoomLevel: zoomLevelReducer,
     splitMapScreen: splitMapScreenReducer,
     mapCenter: mapCenterReducer,
+    isLoading: isLoadingReducer,
   },
 });
