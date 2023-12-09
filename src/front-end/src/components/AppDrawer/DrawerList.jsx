054fc5bb694e290a7043cac033d55419e57de089
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-12-09 11:01:32 +0200

    feat: add basic navigate on drawer list item click

--- src/front-end/src/components/AppDrawer/DrawerList.jsx
+++ src/front-end/src/components/AppDrawer/DrawerList.jsx
@@ -5,12 +5,24 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
+import { useNavigate } from "react-router-dom";
 
 const DrawerList = () => {
+  const navigate = useNavigate();
+
+  const onDrawerListItemClick = () => {
+    navigate("/map");
+  };
+
   return (
     <List>
       {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-        <ListItem key={text} disablePadding sx={{ display: "block" }}>
+        <ListItem
+          key={text}
+          disablePadding
+          sx={{ display: "block" }}
+          onClick={onDrawerListItemClick}
+        >
           <ListItemButton
             sx={{
               minHeight: 48,
