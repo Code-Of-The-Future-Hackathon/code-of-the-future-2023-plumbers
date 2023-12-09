cc63b1063c9e97e092873ef2d6e1c11d707e687c
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-12-09 10:34:26 +0200

    feat: combine all drawer parts for app

--- src/front-end/src/components/AppDrawer.jsx
+++ src/front-end/src/components/AppDrawer.jsx
@@ -1,9 +1,38 @@
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+
+import AppBar from "./AppBar";
+import DrawerHeader from "./DrawerHeader";
+import DrawerList from "./DrawerList";
+import CustomDrawer from "./CustomDrawer";
+
 const AppDrawer = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <div>
-      <h1>AppDrawer</h1>
-    </div>
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar open={open} handleDrawerOpen={handleDrawerOpen} />
+      <CustomDrawer variant="permanent" open={open}>
+        <DrawerHeader handleDrawerClose={handleDrawerClose} />
+        <Divider />
+        <DrawerList />
+      </CustomDrawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <DrawerHeader />
+      </Box>
+    </Box>
   );
-}
+};
 
 export default AppDrawer;
