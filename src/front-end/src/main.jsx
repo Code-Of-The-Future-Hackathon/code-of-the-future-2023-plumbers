b2f18cc7c3d3dfc40190c0ab49c49f65be9bdb3e
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-12-09 08:53:51 +0200

    feat: use redux store and add provider

--- src/front-end/src/main.jsx
+++ src/front-end/src/main.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 import Home from "./pages/Home/Home";
 import Map from "./pages/Map/Map";
+import store from "./redux/store";
 import "./index.css";
 
 const router = createBrowserRouter([
@@ -17,13 +19,15 @@ const router = createBrowserRouter([
     element: <Home />,
   },
   {
-    path: '/map',
-    element: <Map />
-  }
+    path: "/map",
+    element: <Map />,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
