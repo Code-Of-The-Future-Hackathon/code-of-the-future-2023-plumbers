465c040fd329b5fd3e4f9802508a3fe1d1c60e1f
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-12-09 15:07:07 +0200

    feat: create slice for notification

--- src/front-end/src/redux/store.js
+++ src/front-end/src/redux/store.js
@@ -4,6 +4,7 @@ import zoomLevelReducer from "./zoomLevelSlice.js";
 import splitMapScreenReducer from "./splitMapScreenSlice.js";
 import mapCenterReducer from "./mapCenterSlice.js";
 import isLoadingReducer from "./isLoadingSlice.js";
+import notificationReducer from "./notificationSlice.js";
 
 export default configureStore({
   reducer: {
@@ -11,5 +12,6 @@ export default configureStore({
     splitMapScreen: splitMapScreenReducer,
     mapCenter: mapCenterReducer,
     isLoading: isLoadingReducer,
+    notification: notificationReducer,
   },
 });
