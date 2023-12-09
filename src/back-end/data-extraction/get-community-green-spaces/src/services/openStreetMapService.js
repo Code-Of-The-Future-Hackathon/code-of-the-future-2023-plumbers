917fff0a8224ff0df42ea283dad92aad6faa84ec
Daniel Yordanov <dakataebg@students.softuni.bg>
2023-12-09 11:11:36 +0200

    Added getLeisureInRelation function

--- src/back-end/data-extraction/get-community-green-spaces/src/services/openStreetMapService.js
+++ src/back-end/data-extraction/get-community-green-spaces/src/services/openStreetMapService.js
@@ -1,3 +1,20 @@
-function getLeisureInRelationAsync() {}
+const getURL = (queryString) =>
+  `https://overpass-api.de/api/interpreter?${queryString}`;
+
+async function getLeisureInRelationAsync(leisure, relation) {
+  const url = getURL(`[out:json];
+                        rel(${relation});
+                        map_to_area->.a;
+                        (
+                        way(area.a)["leisure"="${leisure}"];
+                        node(area.a)["leisure"="${leisure}"];
+                        );
+                        out geom;`);
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  return data;
+}
 
 module.exports = { getLeisureInRelationAsync };
