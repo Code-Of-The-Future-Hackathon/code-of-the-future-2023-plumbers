cee9cdb7815a6c0b93e29e87293dfdde48e1eaf6
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-12-09 22:23:24 +0200

    feat: add colors constant for green spaces

--- src/front-end/src/constants.js
+++ src/front-end/src/constants.js
@@ -1,8 +1,10 @@
 export const DRAWER_WIDTH = 240;
-export const GREEN_SPACES_TYPES = [
-  "garden",
-  "grass",
-  "grassland",
-  "park",
-  "scrub",
-];
+
+export const GREEN_SPACES_COLORS = {
+  garden: "#228B22", // Forest Green
+  grass: "#7CFC00", // Lawn Green
+  grassland: "#9ACD32", // Yellow-Green
+  park: "#32CD32", // Leaf Green
+  scrub: "#808000", // Olive
+};
+export const GREEN_SPACES_TYPES = Object.keys(GREEN_SPACES_COLORS);
