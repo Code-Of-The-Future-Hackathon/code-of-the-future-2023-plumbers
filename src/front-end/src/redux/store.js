5011707944311145251703993f7876212613e0b6
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-12-09 15:02:05 +0200

    feat: create slice for map center

--- src/front-end/src/redux/store.js
+++ src/front-end/src/redux/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import zoomLevelReducer from "./zoomLevelSlice.js";
 import splitMapScreenReducer from "./splitMapScreenSlice.js";
+import mapCenterReducer from "./mapCenterSlice.js";
 
 export default configureStore({
   reducer: {
     zoomLevel: zoomLevelReducer,
     splitMapScreen: splitMapScreenReducer,
+    mapCenter: mapCenterReducer,
   },
 });
