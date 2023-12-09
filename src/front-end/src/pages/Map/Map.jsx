b27b5cd498048d433503fd90312a9888ead64ba1
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-12-09 14:52:26 +0200

    feat: separate google maps from map component

--- src/front-end/src/pages/Map/Map.jsx
+++ src/front-end/src/pages/Map/Map.jsx
@@ -1,56 +1,12 @@
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
-import { memo, useCallback, useState } from "react";
-import Button from "@mui/material/Button";
-import { useDispatch, useSelector } from "react-redux";
+import { memo } from "react";
 
-import { setSplitMapScreen } from "../../redux/splitMapScreenSlice";
-
-const center = {
-  lat: -3.745,
-  lng: -38.523,
-};
+import GoogleMaps from "./GoogleMaps";
 
 const Map = () => {
-  const { isLoaded } = useJsApiLoader({
-    id: "google-map-script",
-    googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
-  });
-  const [map, setMap] = useState(null);
-  const splitMapScreen = useSelector((state) => state.splitMapScreen);
-  const dispatch = useDispatch();
-
-  const onLoad = useCallback(function callback(map) {
-    const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
-
-    setMap(map);
-  }, []);
-
-  const onUnmount = useCallback(function callback(map) {
-    setMap(null);
-  }, []);
-
-  const onClickGreenSpace = () => {
-    dispatch(setSplitMapScreen(true));
-  };
-
-  return isLoaded ? (
-    <GoogleMap
-      mapContainerStyle={{
-        width: splitMapScreen ? "50%" : "100%",
-        height: "91vh",
-      }}
-      center={center}
-      zoom={10}
-      onLoad={onLoad}
-      onUnmount={onUnmount}
-    >
-      <Button variant="outlined" onClick={onClickGreenSpace}>
-        Open Green Space
-      </Button>
-    </GoogleMap>
-  ) : (
-    <></>
+  return (
+    <div>
+      <GoogleMaps />
+    </div>
   );
 };
 
