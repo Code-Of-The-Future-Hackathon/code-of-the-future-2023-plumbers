ec90b5ac504316d703ccaafff9948bd1367cf88a
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-12-09 11:06:54 +0200

    fix: replace router provider with browser

--- src/front-end/src/main.jsx
+++ src/front-end/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -9,28 +9,15 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import "./index.css";
 
-import Home from "./pages/Home/Home";
-import Map from "./pages/Map/Map";
 import store from "./redux/store";
 import AppDrawer from "./components/AppDrawer/AppDrawer";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/map",
-    element: <Map />,
-  },
-]);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <AppDrawer>
-        <RouterProvider router={router} />
-      </AppDrawer>
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <AppDrawer />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
