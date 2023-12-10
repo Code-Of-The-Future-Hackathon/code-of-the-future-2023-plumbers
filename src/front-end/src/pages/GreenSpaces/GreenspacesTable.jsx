82cc3d3efb64b5c6246ec0aff25ee91800bbda77
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-12-10 09:31:22 +0200

    feat: create new table for greenspaces

--- src/front-end/src/pages/GreenSpaces/GreenspacesTable.jsx
+++ src/front-end/src/pages/GreenSpaces/GreenspacesTable.jsx
@@ -1,137 +1,113 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import {
   Box,
   Table,
-  TableContainer,
-  TableHead,
-  TableRow,
-  TableCell,
   TableBody,
+  TableContainer,
+  Paper,
+  TablePagination,
 } from "@mui/material";
-import { ArrowDropDown, ArrowDropUp } from "@mui/icons-material";
 
 import GreenspacesRow from "./GreenspacesRow";
-const GreenspacesTable = ({records, getNewRecords}) => {
+import GreenspacesTableHead from "./GreenspacesTableHead";
 
+const headCells = [
+  { id: "name", numeric: false, disablePadding: true, label: "Name" },
+  { id: "area", numeric: true, disablePadding: false, label: "Area (sq. m.)" },
+  { id: "type", numeric: false, disablePadding: false, label: "Type" },
+  { id: "zone", numeric: false, disablePadding: false, label: "Zone" },
+];
 
+const GreenspacesTable = ({ records }) => {
+  const [order, setOrder] = useState("asc");
+  const [orderBy, setOrderBy] = useState("name");
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
 
-  const [sortConfig, setSortConfig] = useState({ key: null, direction: 'asc' });
-  const sortedRecords = [...records].sort((a, b) => {
-    if (sortConfig.key === 'area' || sortConfig.key === 'dynamic-area') {
-      const areaA = parseFloat(a.area);
-      const areaB = parseFloat(b.area);
-      return sortConfig.direction === 'asc' ? areaA - areaB : areaB - areaA;
-    } else {
-      if (a[sortConfig.key] < b[sortConfig.key]) {
-        return sortConfig.direction === 'asc' ? -1 : 1;
-      }
-      if (a[sortConfig.key] > b[sortConfig.key]) {
-        return sortConfig.direction === 'asc' ? 1 : -1;
-      }
+  const rows = records.map((record) => {
+    return {
+      id: record.id,
+      name: record.name,
+      area: record.area,
+      type: record.type,
+      zone: record.zone,
+    };
+  });
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const stableSort = (array, comparator) => {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0]);
+      if (order !== 0) return order;
+      return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+  };
+
+  const getComparator = (order, orderBy) => {
+    return order === "desc"
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy);
+  };
+
+  const descendingComparator = (a, b, orderBy) => {
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
     }
     return 0;
-  });
-  const handleSort = (key) => {
-    let direction = 'asc';
-    if (sortConfig.key === key && sortConfig.direction === 'asc') {
-      direction = 'desc';
-    }
-    setSortConfig({ key, direction });
   };
- 
+
   return (
-    <TableContainer
-      component={Box}
-      sx={{ mt: 4, border: "1px solid #e0e0e0", borderRadius: "0.5rem" }}
-    >
-      <Table sx={{ width: "100%" }}>
-        <TableHead>
-          <TableRow
-            sx={{
-              backgroundColor: "#10b981",
-              "& > th": { color: "white", cursor: "pointer" },
-            }}
-          >
-            <TableCell
-              sx={{ fontWeight: "bold", fontSize: "1.2rem" }}
-              onClick={() => handleSort("name")}
-            >
-              Име
-              {sortConfig.key === "name" ? (
-                sortConfig.direction === "asc" ? (
-                  <ArrowDropDown />
-                ) : (
-                  <ArrowDropUp />
-                )
-              ) : null}
-            </TableCell>
-             
-            <TableCell
-              sx={{ fontWeight: "bold", fontSize: "1.2rem" }}
-              onClick={() => handleSort("area")}
-            >
-              Ръчно вписана площ в кв. м.
-              {sortConfig.key === "area" ? (
-                sortConfig.direction === "asc" ? (
-                  <ArrowDropDown />
-                ) : (
-                  <ArrowDropUp />
-                )
-              ) : null}
-            </TableCell>
-            <TableCell
-              sx={{ fontWeight: "bold", fontSize: "1.2rem" }}
-              onClick={() => handleSort("dynamic-area")}
-            >
-              Динамично изчислена площ в кв. м.
-              {sortConfig.key === "dynamic-area" ? (
-                sortConfig.direction === "asc" ? (
-                  <ArrowDropDown />
-                ) : (
-                  <ArrowDropUp />
-                )
-              ) : null}
-            </TableCell>
-            <TableCell
-              sx={{ fontWeight: "bold", fontSize: "1.2rem" }}
-              onClick={() => handleSort("category")}
-            >
-              Категория
-              {sortConfig.key === "category" ? (
-                sortConfig.direction === "asc" ? (
-                  <ArrowDropDown />
-                ) : (
-                  <ArrowDropUp />
-                )
-              ) : null}
-            </TableCell>
-            <TableCell
-              align="center"
-              sx={{ fontWeight: "bold", fontSize: "1.2rem" }}
-            >
-              Зона
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {sortedRecords.length > 0 ? (
-            sortedRecords.map((record) => (
-              <GreenspacesRow
-                key={record.id}
-                record={record}
-                getNewRecords={getNewRecords}
-              />
-            ))
-          ) : (
-            <TableRow>
-              <TableCell align="center" sx={{ fontStyle: "italic" }}>
-                Няма налични записи в момента.
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Paper sx={{ width: "85%", mb: 2, p: "10px 20px" }}>
+      <TableContainer>
+        <Table
+          sx={{ minWidth: 750 }}
+          aria-labelledby="tableTitle"
+          size={"medium"}
+        >
+          <GreenspacesTableHead
+            order={order}
+            orderBy={orderBy}
+            onRequestSort={handleRequestSort}
+            rowCount={rows.length}
+          />
+          <TableBody>
+            {stableSort(rows, getComparator(order, orderBy))
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row, index) => (
+                <GreenspacesRow key={row.id} record={row} />
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
   );
 };
 
