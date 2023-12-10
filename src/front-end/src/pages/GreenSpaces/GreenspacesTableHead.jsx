4aa12a8807f284bfa1163a81f926dbe8dbd49349
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-12-10 09:31:40 +0200

    feat: separate table head

---
+++ src/front-end/src/pages/GreenSpaces/GreenspacesTableHead.jsx
@@ -0,0 +1,51 @@
+import {
+  Box,
+  TableCell,
+  TableHead,
+  TableRow,
+  TableSortLabel,
+} from "@mui/material";
+import { visuallyHidden } from "@mui/utils";
+
+const headCells = [
+  { id: "name", numeric: false, disablePadding: true, label: "Name" },
+  { id: "area", numeric: true, disablePadding: false, label: "Area (sq. m.)" },
+  { id: "type", numeric: false, disablePadding: false, label: "Type" },
+  { id: "zone", numeric: false, disablePadding: false, label: "Zone" },
+];
+
+const GreenspacesTableHead = ({ order, orderBy, onRequestSort }) => {
+  const createSortHandler = (property) => (event) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={"left"}
+            padding={headCell.disablePadding ? "none" : "normal"}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : "asc"}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === "desc" ? "sorted descending" : "sorted ascending"}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+};
+
+export default GreenspacesTableHead;
