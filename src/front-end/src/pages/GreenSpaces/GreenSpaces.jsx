8311238920168dfcf04cd81ed52f8a2ab02f7480
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-12-10 10:58:49 +0200

    feat: filter green spaces by type

--- src/front-end/src/pages/GreenSpaces/GreenSpaces.jsx
+++ src/front-end/src/pages/GreenSpaces/GreenSpaces.jsx
@@ -5,14 +5,30 @@ import { useDispatch } from "react-redux";
 import GreenspacesTable from "./GreenspacesTable";
 import { setIsLoading } from "../../redux/isLoadingSlice";
 import { getGreenspaces } from "../../services/greenspacesService";
+import GreenSpacesTypes from "./GreenSpacesTypes";
+import { GREEN_SPACES_AVAILABLE } from "../../constants";
 
 const GreenSpaces = () => {
   const [records, setRecords] = useState([]);
+  const [visibleRecords, setVisibleRecords] = useState([]);
+  const [selectedTypes, setSelectedTypes] = useState(GREEN_SPACES_AVAILABLE);
   const dispatch = useDispatch();
 
   useEffect(() => {
     getNewRecords();
   }, []);
+
+  useEffect(() => {
+    setVisibleRecords(
+      records.filter((record) => {
+        if (Object.keys(selectedTypes).length === 0) {
+          return true;
+        }
+
+        return selectedTypes[record.type];
+      })
+    );
+  }, [records, selectedTypes]);
 
   const getNewRecords = async () => {
     dispatch(setIsLoading(true));
@@ -32,13 +48,17 @@ const GreenSpaces = () => {
       direction={"column"}
       spacing={2}
       sx={{
-        margin: "5rem 2rem",
+        margin: "2rem",
         padding: "8px 16px",
       }}
       alignItems={"center"}
       justifyContent={"center"}
     >
-      <GreenspacesTable records={records} />
+      <GreenSpacesTypes
+        selectedTypes={selectedTypes}
+        setSelectedTypes={setSelectedTypes}
+      />
+      <GreenspacesTable records={visibleRecords} />
     </Stack>
   );
 };
